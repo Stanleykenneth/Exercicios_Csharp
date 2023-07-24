ab517168aee5a8ab47259feb6586a84a1109ccf9
Kenneth Stanley <kstanley_bass@hotmail.com>
2023-07-24 11:05:16 -0300

    Exercício 02
    Estudo proposto em aula para entrada de Dados via teclado C#.

---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    01 - Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+         mensagem explicativa, conforme exemplos. 
+*/
+
+namespace Exercicios01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma;
+
+            soma = num1 + num2;
+
+            Console.WriteLine("SOMA = " + soma);
+
+
+        }
+    }
+}
