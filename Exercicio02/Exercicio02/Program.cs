e82d7c0868a1e6beee8385f707e20b6e1064c4df
Kenneth Stanley <kstanley_bass@hotmail.com>
2023-07-24 11:16:30 -0300

    Exercício 02
    Alterando e complementando o exercício.

--- Exercicio02/Exercicio02/Program.cs
+++ Exercicio02/Exercicio02/Program.cs
@@ -22,8 +22,25 @@ namespace Exercicios01
             int soma;
 
             soma = num1 + num2;
+            Console.WriteLine();
+
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            int soma1;
+            soma1 = num3 + num4;
+            Console.WriteLine();
+
+            int num5 = int.Parse(Console.ReadLine());
+            int num6 = int.Parse(Console.ReadLine());
+
+            int soma2;
+            soma2 = num5 - num5;
+            Console.WriteLine();
 
             Console.WriteLine("SOMA = " + soma);
+            Console.WriteLine("SOMA = " + soma1);
+            Console.WriteLine("SOMA = " + soma2);
 
 
         }
